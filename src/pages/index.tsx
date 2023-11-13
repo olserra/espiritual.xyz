eccfbc811a4c657c168d79a91e7d4664fc79487d
Otavio Serra <olserra@gmail.com>
2023-11-13 11:07:49 +0000

    Adding faq to landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,14 @@
-import { DemoSection } from "@/components/DemoSection";
 import FeatureSection from "@/components/FeatureSection";
-import Reviews from "@/components/Reviews";
 import LandingPageLayout from "@/components/Layout";
 import Hero from "@/components/Hero";
+import Faq from "@/components/Faq";
 
 const About = () => {
   return (
     <LandingPageLayout title="About">
       <Hero />
       <FeatureSection />
-      <Reviews />
-      <DemoSection />
+      <Faq />
     </LandingPageLayout>
   );
 };

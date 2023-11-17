3b849bc210b2113fa8cfb9e946826446ce674fee
Otavio Serra <olserra@gmail.com>
2023-11-17 00:17:34 +0000

    Removing title on chat

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -158,9 +158,6 @@ export default function Home() {
   return (
     <>
       <div className="mx-auto flex flex-col gap-4">
-        <h1 className="text-2xl font-bold leading-[1.1] tracking-tighter text-center">
-          Chat With Your Docs
-        </h1>
         <main className={styles.main}>
           <div className={styles.cloud}>
             <div ref={messageListRef} className={styles.messagelist}>

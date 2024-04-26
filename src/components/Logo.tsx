7eae4339bd69f536caaa6ebc2c8755102cec85f5
Otavio Serra <os@syndikat7.de>
2024-04-26 11:36:05 +0100

    Changing logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import Image from "next/image";
-import EspiritualLogo from "@/assets/logo2.png";
+import { SiPlanetscale } from "react-icons/si";
 import Link from "next/link";
 
 const Logo = () => {
   return (
     <Link href="/">
       <div className="flex flex-row items-center cursor-pointer">
-        <Image src={EspiritualLogo} alt="Espiritual.xyz logo" width={20} />
+        <SiPlanetscale />
         <p className="sm:text-xl text-xl text-gray-600 font-bold font-pacifico ml-2">
           espiritual.xyz
         </p>
